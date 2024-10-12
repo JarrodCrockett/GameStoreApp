c96a2b0d754f44e54cb7477dd2df8d1f8e16e7a5
Jarrod Crockett <51351859+JarrodCrockett@users.noreply.github.com>
2024-10-11 23:03:55 -0500

    Added entities and db context

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,10 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Data Sorce=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
