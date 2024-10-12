4430e8b7e754f51583c8e1344c76c1b7974eb42c
Jarrod Crockett <51351859+JarrodCrockett@users.noreply.github.com>
2024-10-12 13:21:09 -0500

    Added EF and new database migrations

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbcaec25d4bb30430f9f2819ac2902286d1a05f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c96a2b0d754f44e54cb7477dd2df8d1f8e16e7a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
