c1138743db0d54023d176fc78bdde20f0eb9ac83
Jarrod Crockett <51351859+JarrodCrockett@users.noreply.github.com>
2024-10-13 11:58:46 -0500

    Added new genres endpoint and mapping.

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f5a1df2dc305db73b2afda2385a72fcff9c9507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e90644a46696a2bcd6de739a07ac415fefbc1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
