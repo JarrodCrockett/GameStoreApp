7aac31ca76b7a53a533bb2a317c8bfe94909d3fe
Jarrod Crockett <51351859+JarrodCrockett@users.noreply.github.com>
2024-10-15 22:39:47 -0500

    Added edit game form and logic

--- GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8f1383d382c4db55b5638e6f9824b640b61518e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c296af09c302ca6f52a9dd50d7e30651aa28d12")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
