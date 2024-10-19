2f696d451e0644c4f7440673e0205de151cedcfa
Jarrod Crockett <51351859+JarrodCrockett@users.noreply.github.com>
2024-10-19 11:35:51 -0500

    Added way to save new game in edit form

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 using GameStore.Frontend.Models;
 
 namespace GameStore.Frontend.Clients;
@@ -30,5 +31,23 @@ public class GamesClient
         },
     ];
 
+    private readonly Genre[] genres = new GenresClient().GetGenres();
+
     public GameSummary[] GetGames() => [.. games];
+
+    public void AddGame(GameDetails game)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+        var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+        var GameSummary = new GameSummary
+        {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genre.Name,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+
+        games.Add(GameSummary);
+    }
 }
